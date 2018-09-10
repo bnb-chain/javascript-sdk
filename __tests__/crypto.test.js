77b4dfb014358131ef0fd33aaba517b7d5940111
谢宗匠 <zongjiang.xie@dianrong.com>
2018-09-10 15:26:28 +0800

    enhence cipher

--- __tests__/crypto.test.js
+++ __tests__/crypto.test.js
@@ -1,14 +1,9 @@
 import * as crypto from '../src/crypto';
 
 const privateKey = crypto.generatePrivateKey();
-let keyStore;
+const keyStore = crypto.generateKeyStore(privateKey, '1234567');
 
 describe('crypto', () => {
-  it('generateKeyStore baseon on privateKey', () => {
-    keyStore = crypto.generateKeyStore(privateKey, '1234567');
-    console.log(keyStore);
-  });
-
   it('getPrivateKeyFromKeyStore baseon on keyStore', () => {
     const seed = crypto.getPrivateKeyFromKeyStore(keyStore, '1234567');
     expect(seed).toBe(privateKey);
--- src/crypto/index.js
+++ src/crypto/index.js
@@ -144,7 +144,7 @@ export const generateKeyStore = (privateKey, password) => {
   };
 
   const derivedKey = cryp.pbkdf2Sync(new Buffer(password), salt, kdfparams.c, kdfparams.dklen, 'sha256');
-  const cipher = cryp.createCipher('aes-128-ctr', derivedKey.slice(0, 16), iv);
+  const cipher = cryp.createCipher('aes-256-ctr', derivedKey.slice(0, 16), iv);
   if (!cipher) {
     throw new Error('Unsupported cipher');
   }
@@ -164,7 +164,7 @@ export const generateKeyStore = (privateKey, password) => {
       cipherparams: {
         iv: iv.toString('hex')
       },
-      cipher: 'aes-128-ctr',
+      cipher: 'aes-256-ctr',
       kdf: 'pbkdf2',
       kdfparams: kdfparams,
       mac: mac
