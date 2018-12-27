67dc90fc21f7bad14813dec18a8bbbbcd07d9b88
fiowind <fiowind.zfx@gmail.com>
2018-12-27 11:24:08 +0800

    add eslint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   parserOptions: {
     parser: require.resolve('babel-eslint'),
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   "env": {
     es6: true,
     node: true,
-    browser: true
+    browser: true,
+    jest: true
   },
   "extends": "eslint:recommended",
   "rules": {
