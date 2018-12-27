e9e880578a8dd9c5b6b0784a48a7814cdf1663d2
fiowind <fiowind.zfx@gmail.com>
2018-12-27 11:32:19 +0800

    delete eslint-disable for request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,16 @@
-/* eslint-disable */
-import axios from 'axios';
+import axios from "axios"
 
 class HttpRequest {
   constructor(baseURL){
-    this.httpClient = axios.create({ baseURL });
-  } 
+    this.httpClient = axios.create({ baseURL })
+  }
 
   get(path, params, opts) {
-    return this.request("get", path, params, opts);
+    return this.request("get", path, params, opts)
   }
 
   post(path, body, opts) {
-    return this.request("post", path, body, opts);
+    return this.request("post", path, body, opts)
   }
 
   request(method, path, params, opts) {
@@ -19,29 +18,29 @@ class HttpRequest {
       method,
       url: path,
       ...opts
-    };
+    }
 
     if (params) {
-      if (method === 'get') {
-        options.params = params;
+      if (method === "get") {
+        options.params = params
       } else {
-        options.data = params;
+        options.data = params
       }
     }
 
     return this.httpClient
       .request(options)
       .then(response => {
-        return { result: response.data, status: response.status };
+        return { result: response.data, status: response.status }
       }).catch(err => {
-        console.log(err);
-        const msgObj = err.response && err.response.data && JSON.parse(err.response.data.message);
-        let error = new Error(msgObj.message);
-        error.code = msgObj.code;
-        error.abci_code = msgObj.abci_code;
-        throw error;
-      });
+        console.log(err)
+        const msgObj = err.response && err.response.data && JSON.parse(err.response.data.message)
+        let error = new Error(msgObj.message)
+        error.code = msgObj.code
+        error.abci_code = msgObj.abci_code
+        throw error
+      })
   }
 }
 
-export default HttpRequest;
+export default HttpRequest
