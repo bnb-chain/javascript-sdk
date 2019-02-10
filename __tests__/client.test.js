eb62bdfc99259f7fef84cb387f2b439e338ea4bc
zj <zongjiang.xie@dianrong.com>
2019-02-10 13:06:25 +0800

    update test case

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -6,7 +6,6 @@ const mnemonic = "fragile duck lunch coyote cotton pole gym orange share muscle 
 const keystore = {"version":1,"id":"dfb09873-f16f-48c6-a6b8-bb5a705c47a7","address":"bnc1dxj068zgk007fchefj9n8tq06pcuce5ypqm5zk","crypto":{"ciphertext":"33b7439a8d64d73357dc91f88a6b3a45e7303717664d17daf8e8dc1cc708fa4b","cipherparams":{"iv":"88c726d70cd0437bfdb2312dc60103fc"},"cipher":"aes-256-ctr","kdf":"pbkdf2","kdfparams":{"dklen":32,"salt":"ad10ef544417d4a25914dec3d908882686dd9d793b5c484b76fd5aa575cf54b9","c":262144,"prf":"hmac-sha256"},"mac":"f7cc301d18c97c71741492b8029544952ad5567a733971deb49fd3eb03ee696e"}}
 
 const targetAddress = "tbnb1hgm0p7khfk85zpz5v0j8wnej3a90w709zzlffd"
-// const fromAddress = "bnc1ss57e8sa7xnwq030k2ctr775uac9gjzg6tfrv7"
 
 const getClient = async () => {
   const client = new BncClient("https://testnet-dex.binance.org")
@@ -44,6 +43,7 @@ describe("BncClient test", async () => {
   it("create account with mneomnic", async () => {
     const client = await getClient()
     const res = client.createAccountWithMneomnic()
+    console.log(res)
     expect(res.address).toBeTruthy()
     expect(res.privateKey).toBeTruthy()
     expect(res.mnemonic).toBeTruthy()
@@ -78,7 +78,7 @@ describe("BncClient test", async () => {
   })
 
   it("transfer placeOrder cancelOrder", async () => {
-    jest.setTimeout(10000)
+    jest.setTimeout(50000)
 
     const client = await getClient()
     const addr = crypto.getAddressFromPrivateKey(client.privateKey)
@@ -87,19 +87,18 @@ describe("BncClient test", async () => {
     const sequence = account.result && account.result.sequence
 
     const res = await client.transfer(addr, targetAddress, 1, "BNB", "hello world", sequence)
-    console.log(res)
-    expect(res.result[0].code).toBe(0)
+    expect(res.status).toBe(200)
 
-    const res1 = await client.placeOrder(addr, "NNB-338_BNB", 1, 11, 12, sequence + 1)
-    console.log(res1)
-    expect(res1.result[0].code).toBe(0)
+    await wait(3000)
+
+    const res1 = await client.placeOrder(addr, "ADA.B-B63_BNB", 1, 11, 12, sequence + 1)
+    expect(res1.status).toBe(200)
 
     await wait(3000)
 
     const orderId = `${accCode.toString("hex")}-${sequence + 2}`.toUpperCase()
-    const res2 = await client.cancelOrder(addr, "NNB-338_BNB", orderId, orderId, sequence + 2)
-    console.log(res2)
-    expect(res2.result[0].code).toBe(0)
+    const res2 = await client.cancelOrder(addr, "ADA.B-B63_BNB", orderId, sequence + 2)
+    expect(res2.status).toBe(200)
   })
 
   it("get account", async () => {
