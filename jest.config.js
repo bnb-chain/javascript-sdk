7503d0a132ae61e48f320f8b0f6e68f053e30edc
Rubén Illodo Brea <me@rubenillodo.com>
2020-03-05 11:56:44 +0100

    chore: Jest works with TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-      testMatch: [
-        '**/*test.js?(x)',
-      ],
-      testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-      ],
-      rootDir: '',
-      moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-          '<rootDir>/__mocks__/fileMock.js',
-      },
-      moduleFileExtensions: ['js','json','jsx','node'],
-      testEnvironment: "node"
-  }
+  testMatch: ["**/*test.js?(x)"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  rootDir: "",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node"],
+  testEnvironment: "node"
+}
