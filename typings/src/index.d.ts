747ac480905a55b3dd1791a1fc37313740c91e4f
Rubén Illodo Brea <me@rubenillodo.com>
2020-03-16 16:13:02 +0100

    Cleanup build

--- typings/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as client from "./client";
-declare const BncClient: typeof client.BncClient;
-export default BncClient;
--- typings/src/types/msg.d.ts
+++
@@ -1,8 +0,0 @@
-export interface SignMsg {
-}
-export interface Msg {
-}
-export declare abstract class BaseMsg {
-    abstract getSignMsg(): SignMsg;
-    abstract getMsg(): Msg;
-}
