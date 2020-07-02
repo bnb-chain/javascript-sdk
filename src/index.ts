7307becea0839105b61ea8cdce15eb6568c7d8af
Rubén Illodo Brea <me@rubenillodo.com>
2020-07-02 18:04:05 +0200

    refactor: Cleaned up `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
-import * as amino from "./amino"
-import * as client from "./client"
-import * as crypto from "./crypto"
-import ledger from "./ledger"
-import rpc from "./rpc"
-import Transaction from "./tx"
-import * as types from "./types"
-import * as utils from "./utils"
 import "./declarations"
 
-const { BncClient } = client
+import * as amino from "./amino"
+import { BncClient } from "./client"
+import * as crypto from "./crypto"
+import * as types from "./types"
+import * as utils from "./utils"
 
-export { Transaction, crypto, amino, utils, ledger, rpc, types }
+export { default as ledger } from "./ledger"
+export { default as rpc } from "./rpc"
+export { default as Transaction } from "./tx"
 
-module.exports = BncClient
-module.exports.Transaction = Transaction
-
-module.exports.crypto = crypto
-module.exports.amino = amino
-module.exports.utils = utils
-module.exports.ledger = ledger
-module.exports.rpc = rpc
+export { BncClient, crypto, amino, types, utils }
+export default BncClient
